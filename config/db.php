8793b93b7db4480ddf75a6aff131c27cff155485
Humberto <humberto@2com.com.mx>
2018-02-26 20:45:47 -0600

    Se removieron los archivos para la conexión de base de datos

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=template-core',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
